7de8444a3ed39d76cb2f57a5d200e5f2a92e188a
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-10-26 19:40:36 +0300

    change file_server

--- dj-homeworks/request-handling/file_server/app/urls.py
+++ dj-homeworks/request-handling/file_server/app/urls.py
@@ -1,11 +1,26 @@
-from django.urls import path
+from django.urls import register_converter, path
+from datetime import datetime
+from .views import file_content, FileList
+
 
 # Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
+class DatetimeConverter:
+    regex = '[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}'
+
+    def to_python(self, value):
+        # result = datetime(value.year, value.month, value.day).date()
+        result = datetime.strptime(value, "%Y-%m-%d").date()
+        return result if result is not None else ''
+
+    def to_url(self, value):
+        return value.date()
 
 
-urlpatterns = [
-    # Определите схему урлов с привязкой к отображениям .views.FileList и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),
-    # path(..., name='file_content'),
+register_converter(DatetimeConverter, 'date_conver')
+
+urlpatterns = [# Определите схему урлов с привязкой к отображениям .views.FileList и .views.file_content
+    path('', FileList.as_view(), name='file_list'),  # отображение списка файлов
+    path('<date_conver:date>', FileList.as_view(), name='file_list'),
+    # отображение списка файлов с фильтрацией по дате /2018-01-01/
+    path('file/<str:name>', file_content, name='file_content'),  # отображение отдельных файлов /file_name.txt
 ]
--- dj-homeworks/request-handling/file_server/app/views.py
+++ dj-homeworks/request-handling/file_server/app/views.py
@@ -1,29 +1,43 @@
-import datetime
-
-from django.shortcuts import render
+import os
+from datetime import datetime
+from django.shortcuts import render, render_to_response
 from django.views.generic import TemplateView
+from .settings import FILES_PATH
 
 
 class FileList(TemplateView):
     template_name = 'index.html'
-    
+
+    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
     def get_context_data(self, date=None):
-        # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-        return {
-            'files': [
-                {'name': 'file_name_1.txt',
-                 'ctime': datetime.datetime(2018, 1, 1),
-                 'mtime': datetime.datetime(2018, 1, 2)}
-            ],
-            'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
+        file_list = []
+
+        for file in os.listdir(FILES_PATH):
+            # os.stat возвращает именованный кортеж с атрибутами st_mtime и st_ctime
+            path_to_file = os.stat(os.path.join(FILES_PATH, file))
+            # дата создания файла (из стандартного представления времени)
+            data_file = datetime.fromtimestamp(int(path_to_file.st_ctime)).date()
+            print(data_file)
+            # date = datetime.date().now()
+            date = datetime(2018, 1, 1).date()
+
+            if data_file == date or date == None:
+                file_list.append(
+                    {'name': file,
+                     'ctime': datetime.fromtimestamp(int(path_to_file.st_ctime)).date(), # последний раз, когда были изменены атрибуты файла (разрешения, имя и т.д.)
+                    'mtime': datetime.fromtimestamp(int(path_to_file.st_mtime)).date() #  последний раз, когда было изменено содержание файла
+                     })
+
+        return { 'files':  file_list,
+                'date': date # Этот параметр необязательный
         }
 
 
 def file_content(request, name):
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
-    )
+    with open(os.path.join(FILES_PATH, name), encoding='utf-8') as file:
+        data = file.read()
+
+    # return render(request,'file_content.html',context={'file_name': name, 'file_content': data})
+    return render_to_response('file_content.html', context={'file_name': name, 'file_content': data})
 
