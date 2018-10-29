b7e0e7e9379141c3d8cfa2bbfa3cc752a49b715a
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-10-29 16:06:22 +0300

    change file

--- dj-homeworks/site-form-works/credit_calc/app/forms.py
+++ dj-homeworks/site-form-works/credit_calc/app/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class CalcForm(forms.Form):
     initial_fee = forms.IntegerField(label="Стоимость товара")
-    rate = forms.CharField(label="Процентная ставка")
+    # rate = forms.CharField(label="Процентная ставка") ???? зачем здесь поле CharField ????
+    rate = forms.IntegerField(label="Процентная ставка")
     months_count = forms.IntegerField(label="Срок кредита в месяцах")
 
     def clean_initial_fee(self):
@@ -13,6 +16,26 @@ class CalcForm(forms.Form):
             raise forms.ValidationError("Стоимость товара не может быть отрицательной")
         return initial_fee
 
+
+
     def clean(self):
-        # общая функция валидации
+        # Вызов super().clean() гарантирует, что любая логика проверки в родительских классах поддерживается.
+        cleaned_data = super().clean()
+        rate = cleaned_data.get('rate')
+        months_count = cleaned_data.get('months_count')
+
+        msg = 'Этот параметр не может быть отрицательным' # Сообщение ошибки
+        if not rate or rate < 1:
+            self.add_error('rate', msg)
+        if not months_count or months_count < 1:
+            # raise forms.ValidationError(msg)
+            self.add_error('months_count', msg)
+
+
         return self.cleaned_data
+
+    # Так же надо проверить поля.
+    # Если ожидаются только цифры, то forms.CharField излишен.
+    # def validate_even(value):
+    #     if value is not int:
+    #         raise ValidationError(_('тут должно быть число'), params={'value': value}, )
--- dj-homeworks/site-form-works/credit_calc/app/views.py
+++ dj-homeworks/site-form-works/credit_calc/app/views.py
@@ -1,8 +1,37 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 from .forms import CalcForm
 
 
+# В функции get проверяем данные формы c формы.
+# Для этого передаем их в конструктор CalcForm(request.GET) и сохраняем в переменную.
+# Затем проверяем валидность вызовом функции is_valid() формы.
+
 class CalcView(TemplateView):
-    template_name = "app/calc.html"
+    template_name = 'app/calc.html'
+
+    def get(self, request, *args, **kwargs):
+        form = CalcForm(self.request.GET)
+
+        if form.is_valid():
+            cost = int(self.request.GET['initial_fee'])
+            rate = int(self.request.GET['rate'])
+            months_count = int(self.request.GET['months_count'])
+
+            # Х = (стоимость + стоимость * процентную ставку) / срок в месяцах.
+            total_cost = round((cost + cost * rate) / months_count)
+            result = round(total_cost / 12)
+
+            context = {
+                'form': form,
+                'common_result': total_cost,
+                'result': result
+            }
+
+            return render(request, self.template_name, context)
+
+
+        return render(request, self.template_name, {'form': form})
+
 
